11a5cfcd7992796b156e3d40ed5c8b7783e838f4
mohammedmufleeh <mmt7025@gmail.com>
2022-07-30 12:24:18 +0530

    some changes in Cart:views,context_processors

--- cart/context_processors.py
+++ cart/context_processors.py
@@ -11,7 +11,7 @@ def cart_counter(request):
             cart = Cart.objects.filter(cart_id=_cart_id(request))
             cart_items = CartItem.objects.all().filter(cart=cart[:1])
             for cart_item in cart_items:
-                cart_count += cart_item.quantity
+                cart_count = cart_items.count()
         except Cart.DoesNotExist:
             cart_count = 0
     return dict(cart_count=cart_count)
--- cart/views.py
+++ cart/views.py
@@ -58,7 +58,7 @@ def cart(request,total=0, quantity=0, cart_items=None):
     
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))
-        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True).order_by('product')
         for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
